82f79de71f74e93f23d4b459d68f1c0c7389ac85
Vldr <vldr2212@gmail.com>
2024-04-01 23:19:38 +0300

    create spring project

---
+++ src/main/java/org/education/hillel_springhomework/HillelSpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package org.education.hillel_springhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HillelSpringHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HillelSpringHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/education/hillel_springhomework/HillelSpringHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.education.hillel_springhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HillelSpringHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
